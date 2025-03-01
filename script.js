6eaf81b9cada56ee3ecd328382cc2b56043d41c8
Crayjones <123391229+Crayjones@users.noreply.github.com>
2025-03-01 17:37:46 -0600

    general fix
    - not needed code
    
    - updated og website

--- script.js
+++ script.js
@@ -6,5 +6,3 @@ hamburger.addEventListener('click', () => {
     document.querySelector('.topnav').classList.toggle('active');
     hamburger.querySelector("span").innerHTML = document.querySelector('.topnav').classList.contains('active') ? 'close' : 'menu';
 });
-
-document.documentElement.style.setProperty('color-scheme:', 'light');
